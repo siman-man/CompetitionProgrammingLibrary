901d2eda9d3c89ba40f0d16906bc4723a5f1024d
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-04-12 19:54:46 +0900

    Add extgcd

---
+++ algorithm/NumberTheory/extgcd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int extgcd(int a, int b, int &x, int &y){
+  int d = a;
+
+  if(b != 0){
+    d = extgcd(b, a % b, y, x);
+    y -= (a / b) * x;
+  }else{
+    x = 1;
+    y = 0;
+  }
+
+  return d;
+}
+
+int main(){
+  return 0;
+}
